52c0822c4560f7f1b41ead307ae2908ef8810bfb
Arek Olek <arek@applicake.com>
2011-10-05 17:59:32 +0200

    move tagadapter to tag activity

--- app/src/com/applicake/android/ui/Tag.java
+++ app/src/com/applicake/android/ui/Tag.java
@@ -28,17 +28,21 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -230,6 +234,155 @@ public class Tag extends Activity implements OnItemClickListener {
         view.findViewById(R.id.row_label).startAnimation(mFadeOutAnimation);
       }
     }
+  }
+
+  public class TagListAdapter extends BaseAdapter implements OnTagChangeListener {
+
+    /**
+     * Internal class representing single row entry
+     * 
+     * @author Lukasz Wisniewski
+     */
+    private class Entry {
+      /**
+       * Text to be displayed
+       */
+      String text;
+      /**
+       * Indicates whether the entry has already been added meaning whether it
+       * should be grayed out
+       */
+      boolean added;
+
+      public Entry(String text, boolean added) {
+        this.text = text;
+        this.added = added;
+      }
+    }
+
+    private ArrayList<Entry> mList;
+    private Activity mContext;
+
+    public TagListAdapter(Activity context) {
+      mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+      View row = convertView;
+
+      ViewHolder holder;
+
+      // FIXME caching doesn't work right
+      //      if (row == null) {
+      LayoutInflater inflater = mContext.getLayoutInflater();
+      row = inflater.inflate(R.layout.tag_row, null);
+
+      holder = new ViewHolder();
+      holder.label = (TextView) row.findViewById(R.id.row_label);
+
+      row.setTag(holder);
+      //      } else {
+      //        holder = (ViewHolder) row.getTag();
+      //      }
+
+      // TODO remove hardcoded colors
+      if (mList.get(position).added) {
+        holder.label.setTextColor(0x337a7a7a);
+      } else {
+        holder.label.setTextColor(mContext.getResources().getColorStateList(
+            R.drawable.list_entry_color));
+      }
+
+      holder.label.setText(mList.get(position).text);
+
+      return row;
+    }
+
+    @Override
+    public int getCount() {
+      if (mList != null)
+        return mList.size();
+      else
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+      return mList.get(position).text;
+    }
+
+    @Override
+    public long getItemId(int position) {
+      return position;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+      return !mList.get(position).added;
+    }
+
+    /**
+     * Sets data source (ArrayList of tags) for this adapter
+     * 
+     * @param tags
+     *          ArrayList of tags
+     */
+    public void setSource(ArrayList<String> tags) {
+      setSource(tags, null);
+    }
+
+    /**
+     * Sets data source (ArrayList of tags) for this adapter, additionally
+     * allows to disable so called present tags which could have been previously
+     * added by the user and we want them to be grayed out
+     * 
+     * @param tags
+     *          ArrayList of tags
+     * @param presentTags
+     *          ArrayList of already added tags or null
+     */
+    public void setSource(ArrayList<String> tags, ArrayList<String> presentTags) {
+      mList = new ArrayList<Entry>();
+      for (int i = 0; i < tags.size(); i++) {
+        Entry entry = new Entry(tags.get(i), false);
+        if (presentTags != null) {
+          entry.added = presentTags.contains(tags.get(i));
+        }
+        mList.add(entry);
+      }
+      notifyDataSetChanged();
+    }
+
+    @Override
+    public void onTagAdded(String tag) {
+      updateTag(tag, true);
+    }
+
+    @Override
+    public void onTagRemoved(String tag) {
+      updateTag(tag, false);
+    }
+
+    private int updateTag(String tag, boolean added) {
+      for (int i = 0; i < mList.size(); i++) {
+        if (mList.get(i).text.equals(tag)) {
+          mList.get(i).added = added;
+          notifyDataSetChanged();
+        }
+      }
+      return -1;
+    }
+
+  }
+
+  /**
+   * Holder pattern implementation, performance boost
+   * 
+   * @author Lukasz Wisniewski
+   */
+  static class ViewHolder {
+    TextView label;
   }
 
 }
